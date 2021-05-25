b2a2a29babcbe2a5177ca0d28b923ba257802246
Eugene Makhinya <jesonsev@gmail.com>
2021-05-25 16:48:03 +0100

    datetime

---
+++ datetime/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+      
+      buf := make([]byte, 256) // создаем буфер
+      for {
+          _, err = conn.Read(buf)
+          if err == io.EOF {
+              break
+          }
+          io.WriteString(os.Stdout, fmt.Sprintf("Custom output! %s", string(buf))) // выводим измененное сообщение сервера в консоль
+      }
+}
+
