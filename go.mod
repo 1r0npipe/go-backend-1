81a2649fa24f8db8d7a496f7671b9130c4025f4b
Eugene Makhinya <eugene.makhinya@gmail.com>
2021-05-31 15:19:59 +0100

    file uploads and LS

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/1r0npipe/go-backend-1
+
+go 1.16
---
+++ task4-httpServer/cmd/server/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+ 
+type UploadHandler struct {
+	HostAddr  string
+	UploadDir string
+ }
+
+var (
+	fileSystem = "/tmp/upload"
+)
+
+type File struct {
+	Name string `json:"filename"`
+	Size int	`json:"sizeByte"`
+}
+
+func main() {
+	uploadHandler := &UploadHandler{
+		UploadDir: fileSystem,
+	}
+	http.Handle("/upload", uploadHandler)
+	http.Handle("/ls", uploadHandler)
+	
+	go http.ListenAndServe(":8000",nil)
+	
+	dirToServe := http.Dir(uploadHandler.UploadDir)
+ 
+	fs := &http.Server{
+		Addr:         ":8080",
+		Handler:      http.FileServer(dirToServe),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	fs.ListenAndServe()
+ }
+func (h *UploadHandler) ListDir() {
+
+}
+ func (h *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		fileList, err := PrintFileSystem(os.DirFS(fileSystem))
+		if err != nil {
+			http.Error(w, "Unable to get content of root directory: " + fileSystem, http.StatusBadRequest)
+			return
+		}
+		jsonOut, err := json.Marshal(fileList)
+		if err != nil {
+			http.Error(w, "Unable to do marshall to Json", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintln(w, string(jsonOut))
+		return
+	case http.MethodPost:
+		h.HostAddr = "http://localhost:8080"
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "Unable to read file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+	
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Unable to read file", http.StatusBadRequest)
+			return
+		}
+
+		filePath := h.UploadDir + "/" + header.Filename
+
+		err = ioutil.WriteFile(filePath, data, 0777)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Unable to save file", http.StatusInternalServerError)
+			return
+		}
+		fileLink := h.HostAddr + "/" + header.Filename
+		fmt.Fprintln(w, fileLink)
+	}
+ }
+ 
+func PrintFileSystem(filepath fs.FS) ([]File,error) {
+	var fileList []File
+	var file File
+	err := fs.WalkDir(filepath, ".", func (path string, info fs.DirEntry, err error) error {
+		fileInfo, _ := info.Info()
+		if !info.IsDir() {
+			file.Name = fileInfo.Name()
+			file.Size = int(fileInfo.Size())
+			fileList = append(fileList, file)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error occurs: %v", err)
+	}
+	return fileList, nil
+}
